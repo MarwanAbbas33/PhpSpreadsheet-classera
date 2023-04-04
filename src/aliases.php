a3da99e1a2fcdc847ba9779bc0b8c74c4aea5479
m.abbas <m.abbas@classera.com>
2023-04-04 13:05:59 +0300

    adding class aliases

--- src/aliases.php
+++ src/aliases.php
@@ -1,3 +1,7 @@
 <?php
-class_alias('PhpSpreadsheet\Spreadsheet','PHP_Excel');
-class_alias('PhpSpreadsheet\IOFactory','PHPExcel_IOFactory');
+
+class_alias('PhpOffice\PhpSpreadsheet\Spreadsheet','PHPExcel');
+class_alias('PhpOffice\PhpSpreadsheet\IOFactory','PHPExcel_IOFactory');
+class_alias('PhpOffice\PhpSpreadsheet\Reader\IReadFilter','PHPExcel_Reader_IReadFilter');
+class_alias('PhpOffice\PhpSpreadsheet\Cell\Cell', 'PHPExcel_Cell');
+class_alias('PhpOffice\PhpSpreadsheet\Worksheet\Worksheet', 'PHPExcel_Worksheet');
