f2efa0889e7228c0064cbef7b4006564bde34d46
m.abbas <m.abbas@classera.com>
2023-04-04 14:17:09 +0300

    PHPExcel_Writer_Excel5

--- src/aliases.php
+++ src/aliases.php
@@ -5,3 +5,4 @@ class_alias('PhpOffice\PhpSpreadsheet\IOFactory','PHPExcel_IOFactory');
 class_alias('PhpOffice\PhpSpreadsheet\Reader\IReadFilter','PHPExcel_Reader_IReadFilter');
 class_alias('PhpOffice\PhpSpreadsheet\Cell\Cell', 'PHPExcel_Cell');
 class_alias('PhpOffice\PhpSpreadsheet\Worksheet\Worksheet', 'PHPExcel_Worksheet');
+class_alias('PhpOffice\PhpSpreadsheet\Writer\Xls', 'PHPExcel_Writer_Excel5');
